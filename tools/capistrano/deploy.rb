8cf79c066088be03049829983d860665585875ea
glenn <glenn.profile@gmail.com>
2019-01-10 16:51:04 +0800

    test

--- tools/capistrano/deploy.rb
+++ tools/capistrano/deploy.rb
@@ -78,8 +78,8 @@ namespace :deploy do
   after 'deploy:symlink:release', :update_php_fpm do
     on roles(:app), in: :groups, limit: 3, wait: 10 do
 
-      execute "cd '#{fetch(:deploy_to)}/current' && ls -lhA --time-style=long-iso > /tmp/screenshot && cat /tmp/screenshot"
-      execute "ls -lhA --time-style=long-iso > /tmp/screenshot && cat /tmp/screenshot"
+      execute "cd '#{fetch(:deploy_to)}/current' && ls -lhA --time-style=long-iso > /tmp/screenshot && pwd && cat /tmp/screenshot"
+      execute "ls -lhA --time-style=long-iso > /tmp/screenshot && pwd && cat /tmp/screenshot"
       execute "echo $HOME"
       execute "cd '#{fetch(:deploy_to)}/current' && source $HOME/.phpbrew/bashrc "
       execute "cd '#{fetch(:deploy_to)}/current' && source $HOME/.phpbrew/bashrc && phpbrew use 7.1.23"
